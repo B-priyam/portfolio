c3188bc6611e895435ba77d794672c0a24828cd6
B-priyam <priyam3801h@gmail.com>
2024-12-18 17:13:56 +0530

    initial commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
       >
         <div
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-         bg-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
 
